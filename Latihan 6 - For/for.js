06624287edaf254f55e7c6bb92e3a6b4b06af256
puterih <77377000+puterih@users.noreply.github.com>
2021-07-01 11:21:18 +0700

    Add files via upload

---
+++ Latihan 6 - For/for.js
@@ -0,0 +1,11 @@
+// dibawah ini adalah source code
+for( var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++ ) {
+// Variabel itu sebuah tempat/ wadah yg memiliki nama yg digunakan untuk menyimpan nilai
+// For adalah sebuah keyword lain selain while yg dpt digunakan utk melakukan pengulangan
+	console.log('Hello World ' + nilaiAwal + 'x');
+}
+
+// Console.log fungsinya untuk menampilkan teks Perintah ke Console Javascript/ WEB console
+// console.log juga berfungsi untuk Memberi Tahu kita/ dapat Mengetahui kalau ada kesalahan(error) pada Program Kode Js kita
+// Console.log jg bisa menuliskan perintah sintaks javascript langsung di dalam WEB console
+// fungsi Console.log juga digunakan untuk Debugging pd program kita
