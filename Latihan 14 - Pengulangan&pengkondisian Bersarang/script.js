b17f40f64b95df1c580a90f8ee9aa797dfc7509c
puterih <77377000+puterih@users.noreply.github.com>
2021-07-01 11:47:09 +0700

    Add files via upload

---
+++ Latihan 14 - Pengulangan&pengkondisian Bersarang/script.js
@@ -0,0 +1,43 @@
+// Dibawah ini adalah source code
+
+// var s = '';
+// for( var i = 0; i < 7; i++ ) {
+// 	for( var j = 0; j <= i; j++ ) {
+// 		s += '*';
+// 	}
+// 	s += '\n';
+// }
+// console.log(s);
+
+// yg dibawah ini untuk kebalikannya
+
+// var s = '';
+    // Variabel itu sebuah tempat/ wadah yg memiliki nama yg digunakan untuk menyimpan nilai
+// for( var i = 10; i > 0; i-- ) {
+    // 
+// 	for( var j = 0; j < i; j++ ) {
+// 		s += '*';
+// 	}
+// 	s += '\n';
+// }
+// console.log(s);
+// Console.log fungsinya untuk menampilkan teks Perintah ke Console Javascript/ WEB console
+// console.log berfungsi untuk Memberi Tahu kita/ dapat Mengetahui kalau ada kesalahan(error) pada Program Kode Js kita
+// Console.log jg bisa menuliskan perintah sintaks javascript langsung di dalam WEB console
+// fungsi Console.log juga digunakan untuk Debugging pd program kita
+
+
+// sintaks pengkondisian: if, else if, else if, else, switch
+// if( 4 % 2 == 0) {
+// 	console.log('Mod');
+// } else {
+// 	console.log('false');
+// }
+
+var a = '';
+// Variabel itu sebuah tempat/ wadah yg memiliki nama yg digunakan untuk menyimpan nilai
+for( var h = 0; h < 100; h++ ) {
+// For adalah sebuah keyword lain selain while yg dpt digunakan utk melakukan pengulangan
+// string(karakter kutip '' dan " " dan \')
+	console.log( 4 % 2 == 0);
+}
