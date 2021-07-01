48a295b1c6ade1b0ede854d336ca54c54c335d51
puterih <77377000+puterih@users.noreply.github.com>
2021-07-01 11:12:04 +0700

    Add files via upload

---
+++ Latihan 1- menulis Js/script.js
@@ -0,0 +1,12 @@
+// dibawah ini adalah Source Code
+var x = 10;
+/// Var(Variabel) adalah sebuah tempat/ wadah yg memiliki nama yg digunakan untuk menyimpan nilai
+	console.log('Hello world!');
+// tanda ('' & "") adalah String untuk membungkus data, dan ( \') adalah escape character 
+	console.log('Isi dari variable x adalah ' + x );
+// Console.log fungsinya untuk menampilkan teks Perintah ke Console Javascript/ WEB console
+// console.log juga berfungsi untuk Memberi Tahu kita/ dapat Mengetahui kalau ada kesalahan(error) pada Program Kode Js kita
+// Console.log jg bisa menuliskan perintah sintaks javascript langsung di dalam WEB console
+// fungsi Console.log juga digunakan untuk Debugging pd program kita
+
+	
